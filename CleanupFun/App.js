f08c32b829350fb028a7b772db93947fb27cbb76
Samu <samtobia@gmail.com>
2022-01-26 22:01:00 -0800

    Code Cleanup: Node is not necessary. If I need it I should use typescript

--- CleanupFun/App.js
+++ CleanupFun/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 //import { Main } from "./src/index";
-import type {Node} from 'react';
 import {
   Text,
 } from 'react-native';
 
 import { Main } from "./src/Main";
 
-
-const App: ()=> Node = () => {
+const App = () => {
   return (
     <Main />
   );
