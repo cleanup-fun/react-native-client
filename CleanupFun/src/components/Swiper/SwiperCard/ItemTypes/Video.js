ce234930ad2cf66d374280949468f7519c2db310
Samu <samtobia@gmail.com>
2022-01-26 21:21:56 -0800

    Code Fix: iOS: Video player working with files with spaces

--- CleanupFun/src/components/Swiper/SwiperCard/ItemTypes/Video.js
+++ CleanupFun/src/components/Swiper/SwiperCard/ItemTypes/Video.js
@@ -1,5 +1,6 @@
 
 import React, { useRef, useContext, useEffect } from "react";
+import { Platform } from "react-native";
 import Video from "react-native-video";
 import { MutedContext } from "cleanupfun/src/global-vars/muted";
 
@@ -8,6 +9,8 @@ const FILE_NAME = "/components/Swiper/SwiperCard/ItemTypes/Video.js";
 const logger = new KeyedLogger(FILE_NAME);
 
 import { styles } from "../../styles";
+
+const isFile = /^\//;
 
 export function VideoItem({ fileuri, active }){
   logger.log("video uri:", fileuri, active);
@@ -18,10 +21,17 @@ export function VideoItem({ fileuri, active }){
       player.current.seek(0);
     }
   }, [player, active]);
-  logger.log("active:", active);
+  if(Platform.OS === "ios"){
+    fileuri = fileuri.replace(/ /g, "%20");
+  } else if(Platform.OS === "android"){
+    if(isFile.test(fileuri)){
+      fileuri = "file:/" + fileuri;
+    }
+  }
+  logger.log("active:", fileuri);
   return (
     <Video
-      source={require("cleanupfun/assets/stop-sign.mp4")}
+      source={{ uri: fileuri }}
       ref={player}
       autoplay={true}
       paused={!active}
