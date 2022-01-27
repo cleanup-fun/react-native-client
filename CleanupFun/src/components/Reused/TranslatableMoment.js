39193302c4d5b3a3faa7bc3e62937e65c554be6b
Samu <samtobia@gmail.com>
2022-01-26 21:41:29 -0800

    Code fix: prop is timestamp not markedTimestamp

--- CleanupFun/src/components/Reused/TranslatableMoment.js
+++ CleanupFun/src/components/Reused/TranslatableMoment.js
@@ -14,12 +14,16 @@ Todo:
   - some countries are day month year
   - some countries don't use am/pm but rather 24 hour cycle
 
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/RelativeTimeFormat/RelativeTimeFormat
+
+https://www.i18next.com/translation-function/formatting
+
 */
 
 function TranslateableMoment({ timestamp }){
-  var momentTime = moment(markedTimestamp);
+  var momentTime = moment(timestamp);
   const now = Date.now();
-  if(now - markedTimestamp < WEEK_IN_MILLISECONDS){
+  if(now - timestamp < WEEK_IN_MILLISECONDS){
     return momentTime.fromNow();
   }
   return momentTime.format("dddd, MMMM Do YYYY, h:mm:ss a");
